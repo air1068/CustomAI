9a19d14c37fdeb0a675724315eb679520ead7c7e
air1068 <slashangemon@gmail.com>
2025-04-13 20:49:57 -0500

    Add files via upload

---
+++ CustomAI.cs
@@ -0,0 +1,446 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+using YKF;
+using static UICurrency;
+using static UnityEngine.EventSystems.EventTrigger;
+
+namespace CustomAI {
+    internal static class ModInfo {
+        internal const string Guid = "air1068.elin.customai";
+        internal const string Name = "CustomAI";
+        internal const string Version = "0.1.0";
+        internal const int MagicNumber = (int)(3416877565 % int.MaxValue);
+        //using my last mod's Steam Workshop file ID for a reasonably unique value
+        //(mods don't get a file ID until they're published, so I'm using the previous one instead)
+    }
+
+    internal static class AI {
+        //A value of 0 is saved in RAM but not written to disk, so these have to start at 1.
+        internal const int TARGET = 1;
+        internal const int PLAYER = 2;
+        internal const int SELF = 3;
+
+        internal const int HP = 1;
+        internal const int MP = 2;
+        internal const int SP = 3;
+        internal const int DISTANCE = 4;
+        internal const int STATUS = 5;
+        internal const int SUMMONS = 6;
+
+        internal const int EQUALS = 1;
+        internal const int DOES_NOT_EQUAL = 2;
+        internal const int GREATER_THAN = 3;
+        internal const int LESS_THAN = 4;
+        internal const int GREATER_THAN_OR_EQUALS = 5;
+        internal const int LESS_THAN_OR_EQUALS = 6;
+    }
+
+    [BepInPlugin(ModInfo.Guid, ModInfo.Name, ModInfo.Version)]
+    public class CustomAI : BaseUnityPlugin {
+        private void Awake() {
+            var harmony = new Harmony(ModInfo.Guid);
+            harmony.PatchAll();
+        }
+
+        public void OnStartCore() {
+            var dir = Path.GetDirectoryName(Info.Location);
+            var excel = dir + "/AI_Lang.xlsx";
+            ModUtil.ImportExcel(excel, "General", Core.Instance.sources.langGeneral);
+        }
+    }
+
+    [HarmonyPatch(typeof(Chara), nameof(Chara.SetAIAggro))]
+    class Chara_SetAIAggro_Patch {
+        static bool Prefix(Chara __instance) {
+            if (__instance.GetObj<List<Instruction>>(ModInfo.MagicNumber) != null) {
+                __instance.SetAI(new GoalCustomAICombat());
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(ActPlan), nameof(ActPlan._Update))]
+    class ActPlan__Update_Patch {
+        static void Postfix(PointTarget target, ActPlan __instance) {
+            if (__instance.altAction) {
+                Chara targetNPC = target.pos.FirstVisibleChara();
+                if (targetNPC != null && targetNPC != EClass.pc && EClass.pc.party.members.Contains(targetNPC)) {
+                    __instance.TrySetAct("ActEditCustomAI", delegate {
+                        YK.CreateLayer<CustomUILayer, Chara>(targetNPC);
+                        return true;
+                    }, targetNPC, null, 99);
+                }
+            }
+        }
+    }
+
+    public class CustomUILayer : YKLayer<Chara> {
+        public override void OnLayout() {
+            CreateTab<CustomUITab>("Custom AI Editor", "air1068.elin.customai.tab");
+        }
+        public override Rect Bound { get; } = new Rect(0f, 0f, 1300f, 600f);
+
+        public override void Close() {
+            List<Instruction> instructions = Data.GetObj<List<Instruction>>(ModInfo.MagicNumber);
+            if (instructions.Count > 0) {
+                //prevent save errors by removing the stored UIInputTexts as the UI closes
+                foreach (Instruction i in instructions) {
+                    i.input = null;
+                }
+            } else {
+                //if the last instruction was deleted then remove the empty object
+                Data.mapObj.Remove(ModInfo.MagicNumber);
+            }
+            base.Close();
+        }
+    }
+
+    public class CustomUITab : YKLayout<Chara> {
+        YKGrid instructionsGrid;
+        Chara c;
+        List<string> actions = new List<string>() { "Attack (Melee)", "Attack (Ranged)", "Move (Away)", "Move (Towards)", "And", "Do Nothing" };
+
+        void BuildMenu() {
+            List<Instruction> instructions = c.GetObj<List<Instruction>>(ModInfo.MagicNumber);
+            instructionsGrid.Clear();
+            for (int i = 0; i < instructions.Count; i++) {
+                int tmp = i;
+                instructionsGrid.Dropdown(new List<string> { "Target", "Player", "Self" }, (index) => {
+                    instructions[tmp].entity = index+1;
+                    instructions[tmp].validated = false;
+                }, instructions[tmp].entity-1);
+                instructionsGrid.Dropdown(new List<string> { "HP", "MP", "SP", "Distance", "Status", "Summons" }, (index) => {
+                    instructions[tmp].condition = index+1;
+                    instructions[tmp].validated = false;
+                }, instructions[tmp].condition-1);
+                instructionsGrid.Dropdown(new List<string> { "=", "!=", ">", "<", ">=", "<=" }, (index) => {
+                    instructions[tmp].comparison = index+1;
+                    instructions[tmp].validated = false;
+                }, instructions[i].comparison-1);
+                instructions[tmp].input = instructionsGrid.InputText(instructions[tmp].testvalue, (uselessint) => {
+                    //the UIInputText is stored in the Instruction itself so that its Text can be accessed here
+                    instructions[tmp].testvalue = instructions[tmp].input.Text;
+                    instructions[tmp].validated = false;
+                });
+                instructionsGrid.Dropdown(actions, (index) => {
+                    instructions[tmp].action = actions[index];
+                    instructions[tmp].validated = false;
+                }, actions.FindIndex(a => a == instructions[tmp].action));
+                instructionsGrid.Toggle("Entity=Target", instructions[tmp].PreserveEntityAsTarget, (setting) => {
+                    instructions[tmp].PreserveEntityAsTarget = setting;
+                    instructions[tmp].validated = false;
+                });
+                instructionsGrid.Button("-", () => {
+                    instructions.RemoveAt(tmp);
+                    if (instructions.Count > 0) {
+                        BuildMenu();
+                    } else {
+                        Layer.Close();
+                    }
+                });
+            }
+            instructionsGrid.Button("+", () => {
+                instructions.Add(new Instruction());
+                BuildMenu();
+            });
+        }
+
+        public override void OnLayout() {
+            c = Layer.Data;
+            foreach (ActList.Item item in c.ability.list.items) {
+                actions.Add(item.act.Name);
+            }
+            if (c.GetObj<List<Instruction>>(ModInfo.MagicNumber) == null) {
+                c.SetObj(ModInfo.MagicNumber, new List<Instruction>());
+                c.GetObj<List<Instruction>>(ModInfo.MagicNumber).Add(new Instruction());
+            }
+            instructionsGrid = base.Grid();
+            instructionsGrid.Layout.constraintCount = 7;
+            instructionsGrid.Layout.cellSize = new Vector2(180f, 40f);
+            BuildMenu();
+        }
+    }
+
+    public class GoalCustomAICombat : GoalCombat {
+        public override IEnumerable<Status> Run() {
+            //begin default combat behavior
+            if (EClass._zone.isPeace) {
+                owner.enemy = null;
+                owner.ShowEmo(Emo.happy);
+                yield return Success();
+            }
+            if (EClass.pc.isHidden && owner.isHidden) {
+                owner.enemy = null;
+                yield return Success();
+            }
+            if (owner.enemy.isDead || !owner.enemy.ExistsOnMap || !owner.enemy.pos.IsInBounds) {
+                owner.FindNewEnemy();
+                if (owner.enemy == null) {
+                    yield return Success();
+                }
+            }
+            if (owner.enemy.IsPCFaction && EClass.rnd(5) == 0) {
+                owner.enemy = null;
+                if (owner.enemy.enemy == owner) {
+                    owner.enemy.enemy = null;
+                    owner.enemy.enemy.hostility = owner.enemy.enemy.OriginalHostility;
+                }
+                owner.Say("calmdown", owner);
+                yield return Success();
+            }
+            owner.enemy.enemy?.TrySetEnemy(owner);
+            if (EClass.rnd(20) == 0 && owner.isRestrained) {
+                owner.Talk("restrained");
+            }
+            //end default combat behavior
+            List<Instruction> andqueue = new List<Instruction>();
+            foreach (Instruction i in owner.GetObj<List<Instruction>>(ModInfo.MagicNumber)) {
+                //First, if there's a block of "And" conditions, collect all of them.
+                if (i.action == "And") {
+                    andqueue.Add(i);
+                    continue;
+                }
+                //Then, check every "And" condition at once.
+                //If they're all valid, then the following instruction gets checked as normal.
+                //If any of them is invalid, the following instruction gets ignored.
+                //In either case, the queue is cleared and ready for the next "And" block.
+                if (andqueue.Count > 0) {
+                    if (!andqueue.All(andi => andi.IsValid(owner))) {
+                        andqueue.Clear();
+                        continue;
+                    }
+                    andqueue.Clear();
+                }
+                if (i.IsValid(owner)) {
+                    Chara tc = owner.enemy;
+                    if (i.PreserveEntityAsTarget) {
+                        if (i.entity == AI.SELF) {
+                            tc = owner;
+                        } else if (i.entity == AI.PLAYER) {
+                            tc = EClass.pc;
+                        }
+                    }
+                    if (i.action == "Attack (Melee)") {
+                        owner.UseAbility(ACT.Melee, tc);
+                    } else if (i.action == "Attack (Ranged)") {
+                        owner.UseAbility(ACT.Ranged, tc);
+                    } else if (i.action == "Move (Towards)") {
+                        if (owner.isBlind) {
+                            owner.MoveRandom();
+                        } else {
+                            if (!owner.HasCondition<ConFear>() || owner.Dist(tc) > 2) {
+                                owner.TryMoveTowards(tc.pos);
+                            }
+                        }
+                    } else if (i.action == "Move (Away)") {
+                        if (owner.isBlind) {
+                            owner.MoveRandom();
+                        } else {
+                            owner.TryMoveFrom(tc.pos);
+                        }
+                    } else if (i.action == "Do Nothing") {
+                        owner.UseAbility(ACT.Wait);
+                    } else {
+                        owner.UseAbility(owner.ability.list.items.First(item => item.act.Name == i.action).act, tc);
+                    }
+                    yield return Success();
+                }
+            }
+            Msg.Say(owner.NameSimple + " doesn't know how to respond to the situation based on the instructions you've given " + (owner.IsMale ? "him!" : "her!"));
+            yield return Success();
+        }
+    }
+
+    public class Instruction {
+        public bool validated;
+        public int entity;
+        public int condition;
+        public int comparison;
+        public string testvalue;
+        public string action;
+        public bool PreserveEntityAsTarget;
+        public UIInputText input;
+
+        public Instruction() {
+            validated = false;
+            entity = AI.TARGET;
+            condition = AI.DISTANCE;
+            comparison = AI.GREATER_THAN;
+            testvalue = "1";
+            action = "Move (Towards)";
+            PreserveEntityAsTarget = false;
+        }
+
+        public bool IsValid(Chara owner) {
+            Chara tc = owner.enemy;
+            if (entity == AI.SELF) {
+                tc = owner;
+            } else if (entity == AI.PLAYER) {
+                tc = EClass.pc;
+            }
+            if (!this.validated) {
+                if (entity == AI.SELF && PreserveEntityAsTarget && (action == "Move (Away)" || action == "Move (Towards)")) {
+                    Msg.Say("Invalid move target.");
+                    return false;
+                }
+                if (action != "Attack (Melee)" && action != "Attack (Ranged)" && action != "Move (Away)" && action != "Move (Towards)" && action != "And" && action != "Do Nothing") {
+                    if (!owner.ability.list.items.Any(i => i.act.Name == action)) {
+                        Msg.Say("Action not available: " + action);
+                        return false;
+                    }
+                }
+                if (condition == AI.HP || condition == AI.MP || condition == AI.SP) {
+                    if (!int.TryParse(testvalue, out _)) {
+                        if (testvalue[testvalue.Length - 1] == '%') {
+                            if (!int.TryParse(testvalue.Substring(0, testvalue.Length - 1), out _)) {
+                                Msg.Say("Invalid number: " + testvalue);
+                                return false;
+                            }
+                        }
+                    }
+                }
+                else if (condition == AI.DISTANCE) {
+                    if (!int.TryParse(testvalue, out _)) {
+                        Msg.Say("Invalid number: " + testvalue);
+                        return false;
+                    } else if (int.Parse(testvalue) < 1) {
+                        Msg.Say("Distance too low: " + testvalue);
+                        return false;
+                    }
+                }
+                else if (condition == AI.SUMMONS) {
+                    if (!int.TryParse(testvalue, out _) && testvalue != "100%") {
+                        Msg.Say("Invalid number: " + testvalue);
+                        return false;
+                    } else if (int.Parse(testvalue) < 0) {
+                        Msg.Say("Summon count too low: " + testvalue);
+                        return false;
+                    }
+                }
+                else if (condition == AI.STATUS) {
+                    if (comparison != AI.EQUALS && comparison != AI.DOES_NOT_EQUAL) {
+                        Msg.Say("Status comparison can only be \"=\" or \"!=\".");
+                        return false;
+                    }
+                    if (testvalue != "Spiky" && testvalue != "Suicide Bomb" && !EClass.sources.stats.alias.ContainsKey(testvalue)) {
+                        foreach (var row in EClass.sources.stats.rows) {
+                            if (row.name == testvalue || row.name_JP == testvalue) {
+                                testvalue = row.alias;
+                                break;
+                            }
+                        }
+                        if (!EClass.sources.stats.alias.ContainsKey(testvalue)) {
+                            Msg.Say("Unknown status: " + testvalue);
+                            return false;
+                        }
+                    }
+                }
+                this.validated = true;
+            }
+            int left = 0;
+            float right = 0f;
+            if (condition == AI.HP) {
+                left = tc.hp;
+                if (testvalue[testvalue.Length - 1] == '%') {
+                    right = (float.Parse(testvalue.Substring(0, testvalue.Length - 1)) / 100) * tc.MaxHP;
+                } else {
+                    right = int.Parse(testvalue);
+                }
+            } else if (condition == AI.MP) {
+                left = tc.mana.value;
+                if (testvalue[testvalue.Length - 1] == '%') {
+                    right = (float.Parse(testvalue.Substring(0, testvalue.Length - 1)) / 100) * tc.mana.max;
+                } else {
+                    right = int.Parse(testvalue);
+                }
+            } else if (condition == AI.SP) {
+                left = tc.stamina.value;
+                if (testvalue[testvalue.Length - 1] == '%') {
+                    right = (float.Parse(testvalue.Substring(0, testvalue.Length - 1)) / 100) * tc.stamina.max;
+                } else {
+                    right = int.Parse(testvalue);
+                }
+            } else if (condition == AI.DISTANCE) {
+                left = owner.pos.Distance(tc.pos);
+                right = int.Parse(testvalue);
+            } else if (condition == AI.SUMMONS) {
+                left = EClass._zone.CountMinions(tc);
+                if (testvalue == "100%") {
+                    right = tc.MaxSummon;
+                } else {
+                    right = int.Parse(testvalue);
+                }
+            } else if (condition == AI.STATUS) {
+                if (testvalue == "Spiky") {
+                    foreach (Element e in tc.elements.ListElements()) {
+                        if (e.id == 1221) { //featSpike
+                            left = 1;
+                            break;
+                        }
+                    }
+                } else if (testvalue == "Suicide Bomb") {
+                    foreach (Element e in tc.elements.ListElements()) {
+                        if (e.id == 6410) { //ActSuicide
+                            left = 1;
+                            break;
+                        }
+                    }
+                } else {
+                    foreach (Condition cond in tc.conditions) {
+                        if (cond.id == EClass.sources.stats.alias[testvalue].id) {
+                            left = 1;
+                            break;
+                        }
+                    }
+                }
+                right = 1;
+            }
+            switch (comparison) {
+                case AI.EQUALS:
+                    if (left != right) {
+                        return false;
+                    }
+                    break;
+                case AI.DOES_NOT_EQUAL:
+                    if (left == right) {
+                        return false;
+                    }
+                    break;
+                case AI.GREATER_THAN:
+                    if (left <= right) {
+                        return false;
+                    }
+                    break;
+                case AI.LESS_THAN:
+                    if (left >= right) {
+                        return false;
+                    }
+                    break;
+                case AI.GREATER_THAN_OR_EQUALS:
+                    if (left < right) {
+                        return false;
+                    }
+                    break;
+                case AI.LESS_THAN_OR_EQUALS:
+                    if (left > right) {
+                        return false;
+                    }
+                    break;
+            }
+            return true;
+        }
+    }
+}
